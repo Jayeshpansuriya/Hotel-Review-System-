26d12e9bc7a263aacb4749e42223124cfb7e8b8a
JAYESH dINESHBHAI PANSURIYA <pansuriyajayesh786@gmail.com>
2024-09-28 17:43:34 +0530

    Making the otel service and rating service call by the userservice and in this change we can change the port number also and it will not affect to the procces to get data through the userservice

---
+++ UserServices/src/main/java/com/jd/user/services/external/HotelService.java
@@ -0,0 +1,13 @@
+package com.jd.user.services.external;
+
+import com.jd.user.services.entities.Hotel;
+
+public class HotelService {
+
+    public Hotel getHotel(String hotelId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    
+
+}
--- UserServices/src/main/java/com/jd/user/services/services/impl/UserServiceImpl.java
+++ UserServices/src/main/java/com/jd/user/services/services/impl/UserServiceImpl.java
@@ -1,21 +1,23 @@
 package com.jd.user.services.services.impl;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
-import com.jd.user.services.entities.Rating;
-import com.jd.user.services.entities.User;
-import com.jd.user.services.exceptions.ResourceNotFoundException;
-import com.jd.user.services.repositories.UserRepository;
-import com.jd.user.services.services.UserService;
-
-import org.slf4j.Logger; // Use SLF4J Logger
-import org.slf4j.LoggerFactory; // Correct LoggerFactory
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.UUID;
+import com.jd.user.services.entities.Hotel;
+import com.jd.user.services.entities.Rating;
+import com.jd.user.services.entities.User;
+import com.jd.user.services.exceptions.ResourceNotFoundException;
+import com.jd.user.services.external.HotelService;
+import com.jd.user.services.repositories.UserRepository;
+import com.jd.user.services.services.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -25,6 +27,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private HotelService hotelService; // Add HotelService dependency injection
 
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
@@ -52,12 +57,22 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server: " + userId));
 
         // Fetch ratings of the above user from RATING SERVICE
-        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), Rating[].class);
-        logger.info("{} ", ratingsOfUser);
-
-        List<Rating> ratingsList = Arrays.asList(ratingsOfUser);
-        user.setRatings(ratingsList);
-
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATINGSERVICE/ratings/users/" + user.getUserId(), Rating[].class);
+        
+        logger.info("Ratings: {}", (Object) ratingsOfUser);
+        List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            //api call to hotel service to get the hotel
+            //http://localhost:8082/hotels/1cbaf36d-0b28-4173-b5ea-f1cb0bc0a791
+            //ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTELSERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = hotelService.getHotel(rating.getHotelId());
+            // logger.info("response status code: {} ",forEntity.getStatusCode());
+            //set the hotel to rating
+            rating.setHotel(hotel);
+            //return the rating
+            return rating;
+        }).collect(Collectors.toList());
+        user.setRatings(ratingList); // Set the list of ratings to the user
         return user;
     }
 }
